f48dbb2a8009d98f9bbd8f70b97c79f7d52e3d3b
fidiatus sakinah <fidiatussakinah@gmail.com>
2017-02-04 11:27:37 +0700

    php

---
+++ php.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>PHP</title>
+</head>
+<body>
+<h1>DATA MAHASISWA</h1>
+<form name==”input data” action=”dta1.php” method=”post”>
+<table border=”1?>
+<tr>
+<td>Nama</td>
+<td>:</td>
+<td><input type=”text” name=”nama” /></td>
+</tr>
+<tr>
+<td>Prodi</td>
+<td>:</td>
+<td><select id=”prodi” type="radio" name=”Prodi”>
+<option value=”TK”>Teknik Komputer</option>
+<option value=”MI”>Manajemen Informatika</option>
+</select>
+<tr>
+<td>Jurusan</td>
+<td>:</td>
+<td><select id=”jurusan” name=”jurusan”>
+<option value=”TE”>Teknik Elektro</option>
+<option value=”TS”>Teknik Sipil</option>
+<option value=”Ti”>Teknologi Informasi</option>
+</select>
+</td>
+</tr>
+<tr>
+<td><input type=”submit” name=”submit” value=”submit” /></td>
+</table>
+</body>
+</html>
+<body>
+</body>
+</html>
